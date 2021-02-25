45f03f307f5893ea27aa4fff23e7481e6e77c029
vbilez <d.sqate@gmail.com>
2021-02-26 00:07:44 +0200

    home page first block

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,41 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <header className="top_header ha-waypoint ha-header-small header35">
+
+         <div id="slides" style={{"position":"relative","overflow":"hidden","width":"100%","height":"937px"}}>
+             <div className="slides-control" style={{"position":"relative","height":"100%","width":"100%"}}><ul className="slides-container" style={{"margin":"0px","padding":"0px","listStyle":"none","position":"relative","height":"100%","display":"block"}}>
+
+                 <li style={{"display":"block","left":"1021px","position":"absolute","overflow":"hidden","height":"100%","width":"1021px","top":"0px","zIndex":"2","opacity":"1"}}>
+                     <img src="img/w-1.jpg" alt="4k nature relax tv slider" style={{"backfaceVisibility":"hidden","position":"absolute","left":"-616.5px","top":"0px","zIndex":"-1","maxWidth":"none","height":"937px","width":"2254.44px"}}/>
+                    
+                     <div className="content-slides">
+                         <div className="container">
+                             <div className="row">
+                                 <div className="col-sm-12 col-md-6">
+                                     <div className="container-animation cn-text">
+                                         <h3>Love the Earth – Relax with Nature!</h3>
+                                         <strong>Turn on your TVs and stream relax videos in PREMIUM 4K &amp; 4K HDR quality.</strong>
+                                         <div className="cn-text-style">
+                                              <p>Take a deep breath, escape to beautiful natural destinations and experience nature like never before with a new app from Pro Art inc. An incredible collection of 4K &amp; 4K HDR nature relax videos with more than 1000 hours of sensational nature relax ad-free videos bring natural wonders closer!</p>
+                                         </div>
+                                    </div>
+                               </div>
+                               <div className=" col-sm-12 col-md-6">
+                                   <div id="embed_container" className=" embed_container container-animation cn-video">
+								<div className="video-frame2"><iframe className="black_back" type="text/html" src="https://player.vimeo.com/video/422874793" id="video" width="490" height="274" frameBorder="0" webkitallowfullscreen="" mozallowfullscreen="" allowFullScreen=""></iframe></div>
+                                       
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </li>
+
+
+     </ul></div>
+</div>
+</header>
         </div>
     )
 }
