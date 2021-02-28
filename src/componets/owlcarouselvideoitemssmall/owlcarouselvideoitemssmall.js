ec62b5aebc8bfb32d580cb1065994da1a45f9508
vbilez <d.sqate@gmail.com>
2021-02-28 21:22:48 +0200

    click on small carousel(bg and textchange)

--- src/componets/owlcarouselvideoitemssmall/owlcarouselvideoitemssmall.js
+++ src/componets/owlcarouselvideoitemssmall/owlcarouselvideoitemssmall.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 const OwlCarouselVideoItemsSmall = (props) => {
-   
+   function handleClick(name,link,img,ev){
+       console.log(ev);
+       document.getElementById("free_preview_name").innerHTML=name;
+       document.getElementById("watch_free_preview").setAttribute("href",link);
+      document.getElementsByClassName("free-preview")[0].setAttribute("style",
+      "background-image: url('"+img+"'); display: block;"
+      );
+       console.log(name);
+   };
    return <React.Fragment>{props.videos.map((item) => {
        var itemstyle = item.lock ? 'item-style-lock':'item-style-play';
-    return  <div key={item.id} className="item">
-                                <a href={item.link}>
+    return  <div key={item.id} className="item" onClick={(e) => handleClick(item.name,item.link,item.img, e)}>
+                              
                                     <div className={"c-item-style " + (itemstyle)}>
                                         
                                         <img src={item.img} alt={item.name}/> 
                                     </div>
                                    
-                                </a>
+                             
                             </div>
   })}</React.Fragment>
 }
