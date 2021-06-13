f814e9ec0e51acdfc912e400d691fc17d422439f
web <webdev2021@gmail.com>
2021-06-14 02:44:29 +0300

    business component footer

--- src/pages/business/business.js
+++ src/pages/business/business.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Business = () => {
     return (
         <React.Fragment>
-    <div className="slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" role="tabpanel" id="slick-slide00" style={{"width": "1519px"}} />
+    {/* <div className="slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" role="tabpanel" id="slick-slide00" style={{"width": "1519px"}} />
     <div>
         <div className="main-slider__item" style={{"width": "100%", "display": "inline-block"}} />
             <div className="container-main">
@@ -23,7 +23,153 @@ const Business = () => {
                 <source src="https://player.vimeo.com/external/422874793.hd.mp4?s=5c3ceefbf37e86c5777eeaf5c6830144852fc863&profile_id=175" type="video/mp4" />
                 </video>
             <img src="image/slide_04.jpg" alt="" />
+            </div> */}
+            
+        <div className="main-slider slick-initialized slick-slider slick-dotted" />
+            <div className="slick-list draggable" />
+            <div className="slick-track" style={{"opacity": "1", "width": "13671px", 
+            "transform": "translate3d"}} />
+                <div className="slick-slide slick-cloned" data-slick-index="-1" aria-hidden="true" tabindex="-1" style={{"width": "1519px"}} />
+                    <>
+                    <div className="main-slider__item" style={{"width": "100%", "display": "inline-block"}}>
+                <div className="container-main">
+                    <div className="main-text">
+                        <h2>4K NATURE RELAX TV perfect for businesses of any size and industry</h2>
+                        <p>Keep your customers relaxed and willing to come back more.</p>
+                        <button type="submit" tabindex="-1"><a href="/business#regFormLabel" tabindex="-1">START FREE TRIAL</a></button>
+                    </div>
+                </div>
+                <img src="image/slide_01.jpg" alt="" />
             </div>
+            <div className="slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" role="tabpanel" id="slick-slide00" style={{"width": "1519px"}} aria-describedby="slick-slide-control00" />
+                <div>
+                    <div className="main-slider__item" style={{"width": "100%", "display": "inline-block"}}>
+                <div className="container-main">
+                    <div className="main-text">
+                        <h2>4K NATURE RELAX TV STREAMING SERVICE SPECIALLY DEVELOPED FOR YOUR BUSINESS</h2>
+                        <p>Make your customers and guests feel more comfortable and safe.</p>
+                        <div className="promo">
+                            <i className="fa fa-play"></i>
+                            <p>VIEW PROMO</p>
+                        </div>
+                        <button type="submit" tabindex="0"><a href="/business#regFormLabel" tabindex="0">START FREE TRIAL</a></button>
+                    </div>
+                </div>
+                <span></span>
+                <video className="you" autoplay playsinline loop ><source src="https://player.vimeo.com/external/422874793.hd.mp4?s=5c3ceefbf37e86c5777eeaf5c6830144852fc863&profile_id=175" type="video/mp4" />
+                </video>
+            <img src="image/slide_04.jpg" alt="" />
+            </div></div>
+            <div className="slick-slide" data-slick-index="1" aria-hidden="true" role="tabpanel" id="slick-slide01" style={{"width": "1519px"}} aria-describedby="slick-slide-control01" tabindex="-1" /><div>
+                <div className="main-slider__item" 
+                style={{"width": "100%", "display": "inline-block"}}>
+                <div className="container-main">
+                    <div className="main-text">
+                        <h2>4K NATURE RELAX TV IS THE BEST SOLUTION FOR YOUR BUSINESS.</h2>
+                        <p>Bring nature to your workplace &amp; increase sales.</p>
+                        <button type="submit" tabindex="-1"><a href="/business#regFormLabel" tabindex="-1">START FREE TRIAL</a></button>
+                    </div>
+                </div>
+                <img src="image/slide_02.jpg" alt="" />
+            </div></div>
+            <div className="slick-slide" data-slick-index="2" aria-hidden="true" role="tabpanel" id="slick-slide02" style={{"width": "1519px"}} aria-describedby="slick-slide-control02" tabindex="-1" />
+                <div>
+                    <div className="main-slider__item" 
+            style={{"width": "100%", "display": "inline-block"}} />
+                <div className="container-main" />
+                    <div className="main-text" />
+                        <h2>4K NATURE RELAX TV IS AVAILABLE ON ANY PLATFORM</h2>
+                        <p>Grab your guests’ attention and promote your business</p>
+                        <button type="submit" tabindex="-1"><a href="/business#regFormLabel" tabindex="-1">START FREE TRIAL</a></button>
+                    </div>
+                
+                <img src="image/slide_03.jpg" alt="" />
+            
+            <div className="slick-slide" data-slick-index="3" aria-hidden="true" role="tabpanel" id="slick-slide03" style={{"width": "1519px"}} aria-describedby="slick-slide-control03" tabindex="-1" />
+                <div>
+                    <div className="main-slider__item" 
+                    style={{"width": "100%", 
+                    "display": "inline-block0"}} />
+                <div className="container-main" />
+                    <div className="main-text" />
+                        <h2>4K NATURE RELAX TV perfect for businesses of any size and industry</h2>
+                        <p>Keep your customers relaxed and willing to come back more.</p>
+                        <button type="submit" tabindex="-1"><a href="/business#regFormLabel" tabindex="-1">START FREE TRIAL</a></button>
+                    </div>
+                
+                <img src="image/slide_01.jpg" alt="" />
+            
+            <div className="slick-slide slick-cloned" data-slick-index="4" aria-hidden="true" tabindex="-1" style={{"width": "1519px"}} />
+                <div>
+                <div className="main-slider__item" style={{"width": "100%", "display": "inline-block"}} />
+                <div className="container-main" />
+                    <div className="main-text" />
+                        <h2>4K NATURE RELAX TV STREAMING SERVICE SPECIALLY DEVELOPED FOR YOUR BUSINESS</h2>
+                        <p>Make your customers and guests feel more comfortable and safe.</p>
+                        <div className="promo" />
+                            <i className="fa fa-play"></i>
+                            <p>VIEW PROMO</p>
+                        </div>
+                        <button type="submit" tabindex="-1"><a href="/business#regFormLabel" tabindex="-1">START FREE TRIAL</a></button>
+                
+                <span></span>
+                <video className="you" autoplay playsinline loop />
+                    <source src="https://player.vimeo.com/external/422874793.hd.mp4?s=5c3ceefbf37e86c5777eeaf5c6830144852fc863&profile_id=175" 
+                    type="video/mp4" />
+                    </ >
+            <img src="image/slide_04.jpg" alt="" />
+            
+            
+            <div className="slick-slide slick-cloned" data-slick-index="5" aria-hidden="true" tabindex="-1" style={{"width": "1519px"}} />
+                <div>
+                    <div className="main-slider__item" 
+                    style={{"width": "100%", "display": "inline-block"}} />
+                <div className="container-main" />
+                    <div className="main-text" />
+                        <h2>4K NATURE RELAX TV IS THE BEST SOLUTION FOR YOUR BUSINESS.</h2>
+                        <p>Bring nature to your workplace &amp; increase sales.</p>
+                        <button type="submit" tabindex="-1"><a href="/business#regFormLabel" tabindex="-1">START FREE TRIAL</a></button>
+                    </div>
+            
+                <img src="image/slide_02.jpg" alt="" />
+        
+            <div className="slick-slide slick-cloned" data-slick-index="6" aria-hidden="true" tabindex="-1" style={{"width": "1519px"}} />
+                <div>
+                    <div className="main-slider__item" 
+                    style={{"width": "100%", "display": "inline-block"}} />
+                <div className="container-main" />
+                    <div className="main-text" />
+                        <h2>4K NATURE RELAX TV IS AVAILABLE ON ANY PLATFORM</h2>
+                        <p>Grab your guests’ attention and promote your business</p>
+                        <button type="submit" tabindex="-1"><a href="/business#regFormLabel" tabindex="-1">START FREE TRIAL</a></button>
+                    </div>
+            
+                <img src="image/slide_03.jpg" alt="" />
+            <div className="slick-slide slick-cloned" data-slick-index="7" aria-hidden="true" tabindex="-1" style={{"width": "1519px"}} />
+                <div>
+                    <div className="main-slider__item" 
+                style={{"width": "100%", "display": "inline-block"}} />
+                <div className="container-main" />
+                    <div className="main-text" />
+                        <h2>4K NATURE RELAX TV perfect for businesses of any size and industry</h2>
+                        <p>Keep your customers relaxed and willing to come back more.</p>
+                        <button type="submit" tabindex="-1"><a href="/business#regFormLabel" tabindex="-1">START FREE TRIAL</a></button>
+                    </div>
+            
+                <img src="image/slide_01.jpg" alt="" />
+            <ul className="main-slider-dots" 
+            style={{"display": "flex"}} role="tablist" />
+                <li className="slick-active" role="presentation" />
+                <button type="button" role="tab" id="slick-slide-control00" aria-controls="slick-slide00" aria-label="1 of 4" tabindex="0" aria-selected="true">1</button>
+                <li role="presentation" />
+                    <button type="button" role="tab" id="slick-slide-control01" aria-controls="slick-slide01" aria-label="2 of 4" tabindex="-1">2</button>
+                    
+                    <li role="presentation" className="">
+                        <button type="button" role="tab" id="slick-slide-control02" aria-controls="slick-slide02" aria-label="3 of 4" tabindex="-1">3</button></li>
+                    <li role="presentation" className="">
+                        <button type="button" role="tab" id="slick-slide-control03" aria-controls="slick-slide03" aria-label="4 of 4" 
+                        tabindex="-1">4</button></li>
+    
         </React.Fragment>
     )
 }
